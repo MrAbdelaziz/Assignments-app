1b03694a45e1a7eea277091ca1f23e92d9e5d803
Abdelaziz ELOUAHAB <elouahab.abdelaziz@gmail.com>
2021-03-04 11:59:23 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ClientLayoutComponent} from './client-side/client-layout.component';
+import {DashLayoutComponent} from './dash-board/dash-layout.component';
+import {ClientHomeComponent} from './client-side/contents/client-home/client-home.component';
+import {DashLoginComponent} from './dash-board/contents/dash-login/dash-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'login', component: DashLoginComponent }
+      /*
+      { path: 'home', component: HomeDashComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'user', component: BoardUserComponent },
+      { path: 'mod', component: BoardModeratorComponent },
+      { path: 'admin', component: BoardAdminComponent },
+      { path: 'utilisateurs', component: UsersCrudComponent },
+      { path: 'monuments', component: MonumentComponent },
+      { path: 'regions', component: RegionComponent },
+      { path: 'categories', component: CategorieComponent },
+      { path: 'types', component: TypeComponent }*/
+    ]
+  },
+  {
+    path: 'client',
+    component: ClientLayoutComponent,
+    children: [
+      {path: '', redirectTo: 'home', pathMatch: 'full' },
+      {path: 'home', component: ClientHomeComponent}
+    ]
+  },
+  {path: '**', redirectTo: 'client', pathMatch: 'full' },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientSideComponent } from './client-side/client-side.component';
+import { DashLayoutComponent } from './dash-board/dash-layout.component';
+import { ClientLayoutComponent } from './client-side/client-layout.component';
+import { LoginComponent } from './Dash-Board/contents/login/login.component';
+import { HomeComponent } from './client-side/contents/home/home.component';
+import { ClientHomeComponent } from './client-side/contents/client-home/client-home.component';
+import { DashLoginComponent } from './Dash-Board/contents/dash-login/dash-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientSideComponent,
+    DashLayoutComponent,
+    ClientLayoutComponent,
+    LoginComponent,
+    HomeComponent,
+    ClientHomeComponent,
+    DashLoginComponent
   ],
   imports: [
     BrowserModule,
