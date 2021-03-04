c5a2db149a13e006d2e3eb6a7a2875374ca878d6
Abdelaziz ELOUAHAB <elouahab.abdelaziz@gmail.com>
2021-03-04 12:00:40 +0100

    update

---
+++ src/app/dash-board/contents/dash-login/dash-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-login',
+  templateUrl: './dash-login.component.html',
+  styleUrls: ['./dash-login.component.scss']
+})
+export class DashLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
