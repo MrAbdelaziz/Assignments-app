9272e0ce4b5a5717d2c11d571d0819450a2b33f1
Abdelaziz ELOUAHAB <elouahab.abdelaziz@gmail.com>
2021-03-06 03:56:28 +0100

    update

---
+++ src/app/dashboard/components/main-container/sidebar/matiere-content/matiere-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matiere-content',
+  templateUrl: './matiere-content.component.html',
+  styleUrls: ['./matiere-content.component.scss']
+})
+export class MatiereContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
